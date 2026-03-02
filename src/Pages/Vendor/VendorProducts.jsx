b7df5485be70146698058502ad7443a425522464
Niraj Kumar Mahto <nirajmahto26@gmail.com>
2026-03-02 10:11:51 +0530

    Update import statement for Api.js

--- src/Pages/Vendor/VendorProducts.jsx
+++ src/Pages/Vendor/VendorProducts.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../../services/Api";
+import api from "../../services/Api.js";
 import toast from "react-hot-toast";
 
 function VendorProducts() {
