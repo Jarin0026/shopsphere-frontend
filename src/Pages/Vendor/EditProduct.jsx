17002b557e9d43a2b054524bc64d0d419585e1f9
Niraj Kumar Mahto <nirajmahto26@gmail.com>
2026-03-02 10:12:27 +0530

    Fix import path for Api service in EditProduct

--- src/Pages/Vendor/EditProduct.jsx
+++ src/Pages/Vendor/EditProduct.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import api from "../../services/Api";
+import api from "../../services/Api.js";
 import toast from "react-hot-toast";
 
 function EditProduct() {
